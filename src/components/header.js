dfb239765e18300b5c78a1a19cab389e5cfd418b
James <james.kiesel@gmail.com>
2020-09-07 05:08:32 +1200

    Rename to 'learn'

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = () => {
     <div style={{top: 0, zIndex: 1}} id="header" className="fixed md:static w-full">
       <div className="flex justify-end items-center w-full p-4">
         <a className="mvc-link mr-12" href="#features">{t("header.features")}</a>
-        <a className="mvc-link mr-12" href="/articles">{t("header.articles")}</a>
+        <a className="mvc-link mr-12" href="/learn">{t("header.learning")}</a>
         <a
           onClick={() => {
             trackEvent({ category: 'call-to-action', action: 'create-room', name: 'header' })
