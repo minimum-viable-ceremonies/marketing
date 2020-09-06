40d304d8a7463256c36108b87a469f952d87ffaf
James <james.kiesel@gmail.com>
2020-09-07 05:14:20 +1200

    Basic layout for article page

--- src/pages/article.js
+++ src/pages/article.js
@@ -7,8 +7,10 @@ const ArticlePage = ({ pageContext: { description, html, meta } }) => {
   return (
     <Layout>
       <SEO page="article" meta={meta} />
-      <h1>{description}</h1>
-      <div dangerouslySetInnerHTML={{__html: html}} />
+      <div className="mt-12 m-auto lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm">
+        <h1 className="mb-4">{description}</h1>
+        <div dangerouslySetInnerHTML={{__html: html}} />
+      </div>
     </Layout>
   )
 }
