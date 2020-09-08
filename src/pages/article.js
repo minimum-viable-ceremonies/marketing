39cecffdc36b1f518036feaf17075c4331c2b5c3
James <james.kiesel@gmail.com>
2020-09-08 20:07:14 +1200

    Narrow article width, allow loom embedding

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { document } from "browser-monads"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ArticlePage = ({ pageContext: { description, html, meta } }) => {
+
+  useEffect(() => {
+    document.querySelector('iframe').parentElement.style.opacity = '1'
+    document.querySelector('img.loading-spinner').style.opacity = '0'
+  }, [])
+
   return (
     <Layout>
       <SEO page="article" meta={meta} />
-      <div className="mt-12 m-auto lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm">
+      <div className="mt-12 m-auto max-w-screen-sm">
         <h1 className="mb-4">{description}</h1>
         <div dangerouslySetInnerHTML={{__html: html}} />
       </div>
