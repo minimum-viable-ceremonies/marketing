3799fa50dea5d262fb1e20691bece103c6b631f4
James <james.kiesel@gmail.com>
2020-09-08 20:25:50 +1200

    Don't break articles without loom videos

--- src/pages/article.js
+++ src/pages/article.js
@@ -7,14 +7,16 @@ import SEO from "../components/seo"
 const ArticlePage = ({ pageContext: { description, html, meta } }) => {
 
   useEffect(() => {
-    document.querySelector('iframe').parentElement.style.opacity = '1'
-    document.querySelector('img.loading-spinner').style.opacity = '0'
+    document.querySelectorAll('.article iframe').forEach(({ parentElement }) => {
+      parentElement.style.opacity = "1"
+      parentElement.nextElementSibling.style.opacity = "0"
+    })
   }, [])
 
   return (
     <Layout>
       <SEO page="article" meta={meta} />
-      <div className="mt-12 m-auto max-w-screen-sm">
+      <div className="article mt-12 m-auto max-w-screen-sm">
         <h1 className="mb-4">{description}</h1>
         <div dangerouslySetInnerHTML={{__html: html}} />
       </div>
