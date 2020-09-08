6c0edb60d6722d02116aaebf84099450a4e330a3
James <james.kiesel@gmail.com>
2020-09-08 20:43:06 +1200

    Use ref over document

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from "react"
-import { document } from "browser-monads"
+import React, { useRef, useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ArticlePage = ({ pageContext: { description, html, meta } }) => {
 
+  const articleRef = useRef()
   useEffect(() => {
-    document.querySelectorAll('.article iframe').forEach(({ parentElement }) => {
+    if (!articleRef.current) { return }
+
+    articleRef.current.querySelectorAll('iframe').forEach(({ parentElement }) => {
       parentElement.style.opacity = "1"
       parentElement.nextElementSibling.style.opacity = "0"
     })
-  }, [])
+  }, [articleRef])
 
   return (
     <Layout>
       <SEO page="article" meta={meta} />
-      <div className="article mt-12 m-auto max-w-screen-sm">
+      <div ref={articleRef} className="article mt-24 md:mt-12 m-auto max-w-screen-sm">
         <h1 className="mb-4">{description}</h1>
         <div dangerouslySetInnerHTML={{__html: html}} />
       </div>
