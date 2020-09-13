ec1bebae6ef1fde3bb18a9d5507e008c95967202
James Kiesel <james.kiesel@gmail.com>
2020-09-13 16:28:27 +1200

    Add padding to bottom of article

--- src/components/article.js
+++ src/components/article.js
@@ -9,7 +9,7 @@ const Article = ({ article: { slug, author, timestamp, meta: { title, blurb, ima
   const { t } = useTranslation()
 
   return (
-    <div className="article p-4 mb-24" style={{flexBasis: "50%"}}>
+    <div className="article p-4 pb-24 mb-24" style={{flexBasis: "50%"}}>
       <Link to={`/articles/${slug}`}>
         <div className="article__preview mb-4" style={{backgroundImage: `url(${image})`}} />
         <div className="article__meta mb-4">
