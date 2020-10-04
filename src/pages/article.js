ad59b881628ac8f202c05cf243c9912e09301b25
James <james.kiesel@gmail.com>
2020-10-05 12:07:43 +1300

    Remove old notion hax

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react"
+import React, { useEffect } from "react"
 import { document } from "browser-monads"
 
 import Layout from "../components/layout"
@@ -7,16 +7,6 @@ import SEO from "../components/seo"
 import "../data/locales"
 
 const ArticlePage = ({ pageContext: { slug, html, title, custom_excerpt, featured_image } }) => {
-  const articleRef = useRef()
-  useEffect(() => {
-    if (!articleRef.current) { return }
-
-    articleRef.current.querySelectorAll('iframe').forEach(({ parentElement }) => {
-      parentElement.style.opacity = "1"
-      parentElement.nextElementSibling.style.opacity = "0"
-    })
-  }, [articleRef])
-
   return (
     <Layout>
       <SEO page="article" meta={{
@@ -25,7 +15,7 @@ const ArticlePage = ({ pageContext: { slug, html, title, custom_excerpt, feature
         url: `${document.location.origin}/articles/${slug}`,
         image: featured_image
       }} />
-      <div ref={articleRef} className="article pb-24 mt-24 md:mt-12 m-auto max-w-screen-sm">
+      <div className="article pb-24 mt-24 md:mt-12 m-auto max-w-screen-sm">
         <h1 className="mb-6">{title}</h1>
         <div className="article__content" dangerouslySetInnerHTML={{__html: html}} />
       </div>
