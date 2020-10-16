54ad791b04ffb0e202f2b62c46ccb67129007dee
James <james.kiesel@gmail.com>
2020-10-16 23:32:03 +1300

    Feature image fix

--- src/pages/article.js
+++ src/pages/article.js
@@ -8,6 +8,7 @@ import SEO from "../components/seo"
 import "../data/locales"
 
 const ArticlePage = ({ pageContext: { slug, html, title, custom_excerpt, feature_image, primary_tag } }) => {
+  const { name } = primary_tag || {}
   return (
     <Layout breadcrumb={<Link to="/learn">Back</Link>}>
       <SEO page="article" meta={{
@@ -17,7 +18,7 @@ const ArticlePage = ({ pageContext: { slug, html, title, custom_excerpt, feature
         image: feature_image
       }} />
       <div className="article pb-24 mt-24 md:mt-12 m-auto max-w-screen-sm">
-        {primary_tag.name !== 'update' && <img className="article-hero mb-8 w-full h-auto" alt={title} src={feature_image} />}
+        {feature_image && name !== 'update' && <img className="article-hero mb-8 w-full h-auto" alt={title} src={feature_image} />}
         <h1 className="mb-6">{title}</h1>
         <div className="article__content" dangerouslySetInnerHTML={{__html: html}} />
       </div>
