b8c401a42f0795f8e829646450492d411b5c3aac
James <james.kiesel@gmail.com>
2020-10-17 13:26:34 +1300

    Fix failing test

--- cypress/integration/help.test.js
+++ cypress/integration/help.test.js
@@ -6,6 +6,6 @@ describe('Help page', () => {
   it('Displays help information', () => {
     cy.contains('h1', 'Having trouble?')
     cy.contains('a', 'hey@minimal.cards')
-    cy.contains('#fc_frame')
+    cy.contains('.help__chat')
   })
 })
